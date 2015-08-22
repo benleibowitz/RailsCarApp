9352c5a12d365c9ee4b46fb9d4bc90507d834f80
benleibowitz <benleibowitz2013@gmail.com>
2015-08-22 17:06:25 -0400

    Restyle forms

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,7 +4,7 @@ class Car < ActiveRecord::Base
 	validates :year, :make, :model, :price, :presence => true
 
 	has_attached_file :image
-	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"], :less_than => 2.megabytes, 
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"], :less_than => 2.megabytes
 
 	after_save    :expire_car_all_cache
 	after_destroy :expire_car_all_cache
